dc0ac6cbe950e9e92db14533e59fd1fd43d1059c
jinujo <jinuhjoseph@gmail.com>
2019-06-29 21:40:19 +0530

    Latest patch

--- dynamicformarray/src/app/app.component.ts
+++ dynamicformarray/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
+import { FormGroup, FormControl,FormArray,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dynamicformarray';
+export class AppComponent implements OnInit   {
+  dynamicArrayForm:FormGroup;
+constructor(private fb:FormBuilder){
+this.dynamicArrayForm=this.fb.group({
+  codetype:[''],
+  codevalue:[''],
+  codeitems:this.fb.array([])
+})
+}
+ngOnInit(){
+ 
+}
+CreateItems(){
+  return this.fb.group({
+    code:'',
+    label:''
+  })
+}
 }
